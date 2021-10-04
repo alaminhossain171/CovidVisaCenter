f13e51a01d26b5fd9d0fc1c63642dfcbafd35fae
Md. Al Amin <48113312+alaminhossain171@users.noreply.github.com>
2021-10-04 13:12:35 +0600

    project envionment setup ready
    React bootstrap, font awesome icon, folder structure, react routing complete

---
+++ src/Components/Account/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../Assets/CSS/Account/Home.css';
+
+const Home = () => {
+    return (
+        <div>
+       This is Home components
+        </div>
+    );
+};
+
+export default Home;
